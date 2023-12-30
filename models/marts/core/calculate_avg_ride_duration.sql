0fff4cd5ebfc08311fce3210619f315aa16585ff
Baris Bilen <alibarisbilen1@gmail.com>
2023-12-30 16:42:31 +0000

    reformat model

--- models/marts/core/calculate_avg_ride_duration.sql
+++ models/marts/core/calculate_avg_ride_duration.sql
@@ -1,9 +1,14 @@
-with cycle_hires as (
+with
+    cycle_hires as (
 
-SELECT
-  *
-FROM
-  {{ ref('stg_cycle_hires') }}
-)
+        select
+            start_station_name,
+            end_station_name,
+            avg(duration) as average_ride_duration
+        from {{ ref("stg_cycle_hires") }}
+        group by start_station_name, end_station_name
+        order by count(*) desc, average_ride_duration desc
+
+    )
 
 select * from cycle_hires
