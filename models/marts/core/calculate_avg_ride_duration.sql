59d022c36041ef3ef218e3e645d6572a1530be3d
Baris Bilen <alibarisbilen1@gmail.com>
2023-12-30 18:12:41 +0000

    add the same condition to this model as well

--- models/marts/core/calculate_avg_ride_duration.sql
+++ models/marts/core/calculate_avg_ride_duration.sql
@@ -6,6 +6,7 @@ with
             end_station,
             avg(ride_duration) / 60 as average_ride_duration
         from {{ ref("stg_cycle_hires") }}
+        where ride_duration < 864000 --> exclude the rentals with duration over 10 days, possibly indicating a long term lease or data error
         group by start_station, end_station
         order by average_ride_duration desc
 
